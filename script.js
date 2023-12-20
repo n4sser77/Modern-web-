159de186fa9516f46ccf1fb549d428439ef3659c
n4sser77 <137644060+n4sser77@users.noreply.github.com>
2023-12-20 12:18:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add JavaScript functionality
+document.getElementById('exploreButton').addEventListener('click', function() {
+    alert('Button clicked! You can add more functionality here.');
+});
